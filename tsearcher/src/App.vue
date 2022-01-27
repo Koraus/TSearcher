2808f414eae94c2a003a664cfceb222facfdd516
Koraus <dmytro.kashchynets@gmail.com>
2022-01-28 00:15:21 +0200

    Added modalW

--- tsearcher/src/App.vue
+++ tsearcher/src/App.vue
@@ -1,52 +1,75 @@
 <template>
   <div id="app">
-    <section class="search-section">
-      <h1 class="search-section__title">Search</h1>
-      <p class="search-section__paragraph">all what you need</p>
-      <label class="search-section__search-lable">
-        <input
-          @keydown.enter="serarchRequest()"
-          type="text"
-          v-model="searchInput"
-          class="search-section__input"
-          placeholder="Enter your request"
-        />
-        <button
-          v-on:click.stop="serarchRequest()"
-          class="search-section__search-btn"
-        >
-          Search
-        </button>
-        {{ searchInput }}
-      </label>
-    </section>
-    <ul v-if="searchResponse != undefined">
-      <p v-if="searchResponse.objects.length === 0">нічого не знайдено</p>
-      <li
-        v-for="item in searchResponse.objects"
-        :key="item.id"
-        @click="(selectedItem = item ), OpenPopUp()"
-       
-      >
-        {{ item.searchScore }}
-        <p v-if="item.package.author" style="display: inline-block">
-          {{ item.package.author.name }}
-        </p>
-                <p v-if="item.package.author" style="display: inline-block"> 
-         name: {{ item.package.name }}
-        </p>
-      </li>
-      <PopupWindow
-      :isOpen="isOpen"
-      :selectedItem="selectedItem"
-       @close="ClosePopUp()"
-    />
-    </ul>
+    <b-container>
+      <section class="search-section">
+        <h1 class="search-section__title">Search</h1>
+        <p class="search-section__paragraph">all what you need</p>
 
+        <label class="search-section__search-lable">
+          <b-form-input
+            v-model="searchInput"
+            placeholder="Enter your request"
+            @keydown.enter="serarchRequest()"
+            class="m-1"
+          >
+          </b-form-input>
+          <b-button
+            v-on:click.stop="serarchRequest()"
+            variant="outline-primary"
+            class="m-1"
+            >Search
+          </b-button>
+        </label>
+      </section>
+
+      <div v-if="searchResponse" class="search-section__search-result">
+        <div
+          class="card search-section__card"
+          style="width: 100%; max-width: 70rem"
+          v-for="item in searchResponse.objects"
+          :key="item.id"
+          @click="selectedItem = item"
+        >
+          <div class="card-header">
+            <h4>{{ item.package.name }}</h4>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+              <span v-if="item.package.author">
+                <span class="card__list-title">author name:</span>
+                {{ item.package.author.name }}
+              </span>
+              <span v-else> no author name </span>
+            </li>
+            <li class="list-group-item">
+              <span class="card__list-title">description:</span>
+              <span v-if="item.package.description">
+                {{ item.package.description }}
+              </span>
+            </li>
+            <li class="list-group-item">
+              <span class="card__list-title">date:</span>
+              <span v-if="item.package.date"> {{ item.package.date }} </span>
+            </li>
+          </ul>
+
+        </div>
+        <div v-if="searchResponse.total === 0">Нічого не знайдено</div>
+
+          <PopupWindow
+            v-if="selectedItem"
+            :selectedItem="selectedItem"
+            @close="selectedItem = undefined"
+            @click="log(this.selectedItem)"
+          />
+
+      </div>
+    </b-container>
   </div>
 </template>
 
 <script>
+
 import PopupWindow from "./components/PopupWindow.vue";
 
 const apiUrl = "https://registry.npmjs.com/-/v1/";
@@ -61,28 +84,29 @@ export default {
       searchInput: "",
       selectedItem: undefined,
       searchResponse: undefined,
-      isOpen: false,
+      totalPages: undefined,
+      currentPage: 1,
+    
     };
+  },
+  computed: {
     
   },
-  computed: {},
   methods: {
     async serarchRequest() {
       const qResultSize = 10;
+      let startFrom = 100;
       this.searchResponse = await (
         await fetch(
-          `${apiUrl}search?text=${this.searchInput}&size=${qResultSize}`
+          `${apiUrl}search?text=${this.searchInput}&size=${qResultSize}&from=${startFrom}`
         )
       ).json();
+      this.totalPages = Math.ceil(this.searchResponse.total / qResultSize);
+      this.currentPage = Math.ceil(startFrom / qResultSize);
       console.log(this.searchResponse);
-    },
-    OpenPopUp () {
-      this.isOpen = true; 
-    },
-    ClosePopUp(){
-      this.isOpen = false; 
-    },
+      console.log(this.totalPages);
 
+    },
     log(itm) {
       console.log(itm);
     },
@@ -98,5 +122,41 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.search-section__search-lable {
+  display: flex;
+  width: 70%;
+  margin-bottom: 20px;
+}
+.search-section {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.search-section__item {
+  max-width: 800px;
+  min-width: 200px;
+  padding: 20px 0 20px 0;
+  margin: 10px;
+  border-top-width: auto;
+}
+.search-section__search-result {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+.search-section__card {
+  margin: 10px;
+}
+
+.card__list-title {
+  font-size: 14px;
+  color: #2c3e50;
 }
 </style>
--- tsearcher/src/components/PopupWindow.vue
+++ tsearcher/src/components/PopupWindow.vue
@@ -1,18 +1,37 @@
 <template  >
-  <div v-if="isOpen" class="bg-drop" @click.stop="close()">
-    <div v-if="selectedItem" class="base-info">
-      <p style="color: red">Name: {{ selectedItem.package.name }}</p>
-      <p style="color: red">Name: {{ selectedItem.package.description }}</p>
-      <ul>
-        <li v-for="(value, name) in selectedItem.package.links" :key="name.id">
-          {{ name }}: {{ value }}
-        </li>
-      </ul>
+  <div class="bg-drop" @click.stop="close()">
+    <div
+      class="card text-white bg-light  base-info"
+      style="max-width: 60%"
+      v-if="selectedItem"
+    >
+      <div class="card-header"><h3 class="card__title">About</h3></div>
+      <div class="card-body">
+        <p style="color: red">Name: {{ selectedItem.package.name }}</p>
+        <p style="color: red">
+          Description: {{ selectedItem.package.description }}
+        </p>
+        <ul class="card__links-list">
+          <li
+            v-for="(value, name) in selectedItem.package.links"
+            :key="name.id"
+          >
+            {{ name }}: <a target="_blank" :href="value"> {{ value }} </a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div v-if="!packageInfo" class="jsdelivr-info">loading</div>
-    <div v-if="packageInfo" class="jsdelivr-info">
-      <p v-if="isOpen">Last V: {{this.packageInfo.versions[0]}}</p>
-      <p v-if="isOpen"> {{this.packageVersionInfo}} </p>
+    <div class="card text-white bg-light base-info" style="max-width: 60%">
+      <div class="card-header"><h3 class="card__title"> More info about versions <span  v-if="packageInfo"> {{this.packageInfo.versions[0]}}  </span>  </h3></div>
+      <div class="card-body text-success">
+        <p class="card-text" v-if="packageInfo"> </p>
+        <ul class="card__list">
+          File list:
+        <li v-for="item in this.packageVersionInfo.files" :key="item.id"> {{item.name}} </li>
+        </ul>
+        <h5 v-if="!packageInfo" class="card-title"> loading...</h5>
+        
+      </div>
     </div>
   </div>
 </template>
@@ -21,10 +40,9 @@
 export default {
   name: "PopupWindow",
 
-  beforeUpdate() {
-    this.getInfo(); 
-  },  
-
+  created() {
+    this.getInfo();
+  },
 
   data() {
     return {
@@ -34,17 +52,11 @@ export default {
   },
   props: {
     item: Object,
-    isOpen: {
-      type: Boolean,
-      required: true,
-    },
     selectedItem: Object,
   },
   emits: {
     close: null,
   },
-
-
 
   methods: {
     close() {
@@ -61,7 +73,10 @@ export default {
           `https://data.jsdelivr.com/v1/package/npm/${this.selectedItem.package.name}@${this.packageInfo.versions[0]}`
         )
       ).json();
+      
+      
     },
+    
   },
 };
 </script>
@@ -71,17 +86,41 @@ export default {
   background: rgba(0, 0, 0, 0.452);
   min-width: 100%;
   min-height: 100%;
-  position: absolute;
+  position: fixed;
   top: 0;
+  left: 0;
   color: blue;
   font-size: 18px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 40px;
 }
 .base-info {
   background: white;
   width: 70%;
   height: 60%;
+  color: blue;
 }
 .jsdelivr-info {
   background: rgb(48, 197, 140);
+}
+.card__links-list {
+  color: blue;
+ 
+}
+.card__list{
+  
+}
+.card__list > li{
+ text-align: left;
+}
+
+
+.card__links-list > li{
+ text-align: left;
+}
+.card__title{
+  color: royalblue
 }
 </style>
